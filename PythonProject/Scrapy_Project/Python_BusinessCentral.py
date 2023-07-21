77ac05911fbd6b1e1fab7525c11ec35e31836e61
Minh Rieckermann <62591028+MinhRieckermann@users.noreply.github.com>
2023-07-21 14:23:50 +0700

    Update Python_BusinessCentral.py
    Minh modify

--- PythonProject/Scrapy_Project/Python_BusinessCentral.py
+++ PythonProject/Scrapy_Project/Python_BusinessCentral.py
@@ -29,11 +29,11 @@ def GetAccessToken(client_id, client_secret, refresh_token):
  # 
  #    
 
-base_url = "https://api.businesscentral.dynamics.com/v2.0/e3c37df3-5a5e-400f-bf9e-505bf9526fed/JRDEV/api/v2.0/"
+base_url = ""
 #api_endpoint = "companies"
-Refresh_Token = "0.AVYA833D415aD0C_nlBb-VJv7Sf4Ffq_9XtKtPeoITlbqkdWAGg.AgABAAEAAAD--DLA3VO7QrddgJg7WevrAgDs_wUA9P8Z-rdVvJDafX2bmP4VWi2G2AMnCxbqycOqwVpJmFcwtsoMS7GbhN5CIo8sQaeYCbi-iOh5shJ2GLV-X-zOWLQIW2nxe707TsRb_9KvkZuvYZdVOJFHKfi_wer8W4wqPs0zR4ASGDiAm5afDo1wXaf-LGm1pRrOY_Rm03Ppzl_hCO8uuA8f7a1gEqxo1vMehVk3CXuylHs0y6zLs-du3QjDHhUtDhVL9UnZTIJFpaJCKPEzEfL2Gs2fzr4t3C96J3lSMJ4VrZbvYFEorj_A5aYrqRPmfj_hIAeavO1Nar66YV_lGXqkOU5kRbcP-bkJxkzaSBYJUJO8jT5BUpYEnR3uJnP1hG4grqgY4U-B7q6LyOCo5GDRs7aRpq8HCP-HPrNfxsymQDjiXM_zJsGcUqGs5juzYqqAJdxFu8Zlotpzbze8CKXn5-EUFGs7rsEoZiUxcqOcF-UhFstdbf_Za58swIIvcA-2wQeDf1rDWUsWMtjeLFSCO74JuBcKUYYniVne64If4d5fUlHsd5_5X-wy4KWwfOHQax8omF0MtaU1jYolnBXmjOmHHjMnMMlY5a6e8YmyfXncVW6dkK7eGHYaolv_OdvtW6QPIhvzKN5IzNtlg_HKxVADaWPmbf1Ijcy3RrHh9OciopZ6gL0oT935Osn8oRWE6H1CLt9OgdAGa_I0VYG_QWid9MHb5g0Da6wx7aNq4noG29Dt0LsO4Lds89WXNnlvUW0ths1doV7QI09a4Uqw3UQn031KGVPq-L9XGMz2g6w3EImbAlJCBgCApJqzyaAhXRv4S1mTLg"
-client_id="fa15f827-f5bf-4a7b-b4f7-a821395baa47"
-client_secret="JQy8Q~9gnSxrg6g3xMNjHaZhQF-xF9Gnvy_YrcK8"
+Refresh_Token = ""
+client_id=""
+client_secret=""
 
 access_token=GetAccessToken(client_id,client_secret,Refresh_Token)
 headers = {
